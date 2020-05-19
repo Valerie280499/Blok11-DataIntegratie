42cecff32e95c20e71aa5ff3327c793fb85d0ebb
valerie verhalle <v.verhalle@kpnmail.nl>
2020-05-19 19:41:55 +0200

    hello world working on localhost:8080/Interface

---
+++ src/main/java/com/Interface/HelloController.java
@@ -0,0 +1,14 @@
+package com.Interface;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/Interface")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
--- src/test/java/com/Interface/ApiApplicationTests.java
+++ src/test/java/com/Interface/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.API;
+package com.Interface;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
